be9b7d839eb6d1e9ab5ef8a0e4e5a6336d1ea3f5
vtorres96 <victorcdc96@gmail.com>
2020-05-15 18:43:28 -0300

    added database connection variables

---
+++ src/config/database.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+module.exports = {
+    dialect: process.env.DB_PROVIDER,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+}
+
