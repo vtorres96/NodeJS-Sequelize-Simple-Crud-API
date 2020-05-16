35653fc5d94aa38fff5b6e1a03d11d19c3c6f8a3
vtorres96 <victorcdc96@gmail.com>
2020-05-16 08:25:20 -0300

    added authenticate function

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,11 +1,16 @@
 const { User } = require('../models/');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+const authConfig = require("../../config/auth"); 
 
 module.exports = {
 	async index(req, res) {
 		await User.findAll()
 		.then(users => {
-			return res.status(200).json(users)
+			return res.status(200).json({
+				users,
+				loggedUser: req.userId
+			});
 		})
 		.catch(error => {
 			return res.status(404).json(error)
@@ -30,17 +35,45 @@ module.exports = {
 			email,
 			password_hash
 		} = req.body;
-
+		
 		await User.create({
 			email: email,
 			password_hash: bcrypt.hashSync(password_hash, 10)
 		})
 		.then(user => {
-			return res.status(200).json(user);
+			user.password_hash = undefined;
+
+			const token = jwt.sign({ id: user.id }, authConfig.secret, {
+				expiresIn: authConfig.expiresIn
+			});
+	
+			res.status(200).json({ user, token });
 		})
 		.catch(error => {
 			return res.status(400).json('Falha ao processar requisição. ' + error);
 		});
+	},
+
+	async authenticate(req, res) {
+		const { email, password_hash } = req.body;
+		
+		const user = await User.findOne({ where: { email: email } });
+
+		if (!user) {
+			res.status(400).json({ error: 'Usuário não encontrado' });
+		}
+
+		if (!await bcrypt.compareSync(password_hash, user.password_hash)) {
+			res.status(400).json({ error: 'Usuário ou senha inválidos' });
+		} 
+
+		user.password_hash = undefined;
+
+		const token = jwt.sign({ id: user.id }, authConfig.secret, {
+			expiresIn: authConfig.expiresIn
+		})
+
+		res.status(200).json({ user, token })
 	},
 
 	async update(req, res) {
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const authMiddleware = require("../app/middlewares/auth");
 const UserController = require('../app/controllers/UserController');
 
 const routes = express.Router();
 
+routes.post('/user/create', UserController.create);
+
+routes.post('/user/authenticate', UserController.authenticate);
+
+routes.use(authMiddleware);
+
 routes.get('/users/', UserController.index);
 routes.get("/user/:id", UserController.searchById);
-routes.post('/user/create', UserController.create);
+
 routes.put('/user/update/:id', UserController.update);
+
 routes.delete('/user/delete/:id', UserController.delete);
 
 module.exports = routes;
